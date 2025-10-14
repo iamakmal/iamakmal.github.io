bd5401e4988e904228c8bb702c4d59ec49d4e483
iamakmal <m.akmalalavi@gmail.com>
2025-10-14 17:21:42 +0530

    fix: added chips for tools in the project

---
+++ src/components/chip.jsx
@@ -0,0 +1,30 @@
+import { Box, Typography } from "@mui/material";
+
+import React from "react";
+
+const Chip = ({ index, name }) => {
+  const colors = [
+    "#1976d2",
+    "#dc004e",
+    "#ff9800",
+    "#9c27b0",
+    "#00acc1",
+    "#c2185b",
+  ];
+
+  return (
+    <Box
+      sx={{
+        borderRadius: "16px",
+        padding: "4px 12px",
+        display: "inline-block",
+        margin: "1px",
+        backgroundColor: `${colors[index % colors.length]}33`, // 20% opacity
+      }}
+    >
+      <Typography variant="body2">{name}</Typography>
+    </Box>
+  );
+};
+
+export default Chip;
